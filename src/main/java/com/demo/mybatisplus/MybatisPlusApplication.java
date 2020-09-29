6fff22d095515f8ca3b798cd7e170f8dddbcda2d
zengrui <772701414@qq.com>
2020-09-29 16:07:05 +0800

    增加文件上传功能

--- src/main/java/com/demo/mybatisplus/MybatisPlusApplication.java
+++ src/main/java/com/demo/mybatisplus/MybatisPlusApplication.java
@@ -1,9 +1,14 @@
 package com.demo.mybatisplus;
 
+import com.demo.mybatisplus.config.FileStorageProperties;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+@EnableConfigurationProperties({
+        FileStorageProperties.class
+})
 @SpringBootApplication
 @MapperScan(basePackages= {"com.demo.mybatisplus.mapper"})
 public class MybatisPlusApplication {
---
+++ src/main/java/com/demo/mybatisplus/exception/FileStorageException.java
@@ -0,0 +1,15 @@
+package com.demo.mybatisplus.exception;
+
+/**
+ * @Author: 曾睿
+ * @Date: 2020/6/3 09:33
+ */
+public class FileStorageException extends RuntimeException {
+    public FileStorageException(String message) {
+        super(message);
+    }
+
+    public FileStorageException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
